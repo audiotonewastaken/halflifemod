aa982945962e2a218b2dea2e58835fe154d135be
AudioCypher <78041811+audiotonewastaken@users.noreply.github.com>
2023-10-14 08:34:30 -0700

    Add files via upload

---
+++ usp.cpp
@@ -0,0 +1,200 @@
+/***
+*
+*	Copyright (c) 1996-2001, Valve LLC. All rights reserved.
+*	
+*	This product contains software technology licensed from Id 
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
+*	All Rights Reserved.
+*
+*   Use, distribution, and modification of this source code and/or resulting
+*   object code is restricted to non-commercial enhancements to products from
+*   Valve LLC.  All other use, distribution, or modification is prohibited
+*   without written permission from Valve LLC.
+*
+****/
+
+#include "extdll.h"
+#include "util.h"
+#include "cbase.h"
+#include "monsters.h"
+#include "weapons.h"
+#include "player.h"
+
+LINK_ENTITY_TO_CLASS(weapon_usp, CUsp);
+
+
+void CUsp::Spawn()
+{
+	Precache();
+	m_iId = WEAPON_USP;
+	SET_MODEL(ENT(pev), "models/w_usp.mdl");
+
+	m_iDefaultAmmo = USP_DEFAULT_GIVE;
+
+	FallInit(); // get ready to fall down.
+}
+
+
+void CUsp::Precache()
+{
+	PRECACHE_MODEL("models/v_usp.mdl");
+	PRECACHE_MODEL("models/w_usp.mdl");
+	PRECACHE_MODEL("models/p_usp.mdl");
+
+	m_iShell = PRECACHE_MODEL("models/shell.mdl"); // brass shell
+
+	PRECACHE_SOUND("items/9mmclip1.wav");
+	PRECACHE_SOUND("items/9mmclip2.wav");
+
+	PRECACHE_SOUND("weapons/pl_gun1.wav"); //silenced handgun
+	PRECACHE_SOUND("weapons/pl_gun2.wav"); //silenced handgun
+	PRECACHE_SOUND("weapons/pl_gun3.wav"); //handgun
+
+	m_usFireUsp = PRECACHE_EVENT(1, "events/usp.sc");
+}
+
+bool CUsp::GetItemInfo(ItemInfo* p)
+{
+	p->pszName = STRING(pev->classname);
+	p->pszAmmo1 = "9mm";
+	p->iMaxAmmo1 = _9MM_MAX_CARRY;
+	p->pszAmmo2 = NULL;
+	p->iMaxAmmo2 = -1;
+	p->iMaxClip = USP_MAX_CLIP;
+	p->iSlot = 1;
+	p->iPosition = 3;
+	p->iFlags = 0;
+	p->iId = m_iId = WEAPON_USP;
+	p->iWeight = USP_WEIGHT;
+
+	return true;
+}
+
+bool CUsp::Deploy()
+{
+	// pev->body = 1;
+	return DefaultDeploy("models/v_usp.mdl", "models/p_usp.mdl", USP_DRAW, "onehanded");
+}
+
+
+void CUsp::PrimaryAttack()
+{
+	UspFire(0.01, 0.001, true);
+}
+
+void CUsp::UspFire(float flSpread, float flCycleTime, bool fUseAutoAim)
+{
+	if (m_iClip <= 0)
+	{
+		if (m_fFireOnEmpty)
+		{
+			PlayEmptySound();
+			m_flNextPrimaryAttack = GetNextAttackDelay(0.2);
+		}
+
+		return;
+	}
+
+	if (m_pPlayer->m_afButtonLast & IN_ATTACK)
+	{
+		return;
+	}
+
+	m_iClip--;
+
+	m_pPlayer->pev->effects = (int)(m_pPlayer->pev->effects) | EF_MUZZLEFLASH;
+
+	int flags;
+
+#if defined(CLIENT_WEAPONS)
+	flags = FEV_NOTHOST;
+#else
+	flags = 0;
+#endif
+
+	// player "shoot" animation
+	m_pPlayer->SetAnimation(PLAYER_ATTACK1);
+
+	// silenced
+	if (pev->body == 1)
+	{
+		m_pPlayer->m_iWeaponVolume = QUIET_GUN_VOLUME;
+		m_pPlayer->m_iWeaponFlash = DIM_GUN_FLASH;
+	}
+	else
+	{
+		// non-silenced
+		m_pPlayer->m_iWeaponVolume = QUIET_GUN_VOLUME;
+		m_pPlayer->m_iWeaponFlash = DIM_GUN_FLASH;
+	}
+
+	Vector vecSrc = m_pPlayer->GetGunPosition();
+	Vector vecAiming;
+
+	if (fUseAutoAim)
+	{
+		vecAiming = m_pPlayer->GetAutoaimVector(AUTOAIM_10DEGREES);
+	}
+	else
+	{
+		vecAiming = gpGlobals->v_forward;
+	}
+
+	Vector vecDir;
+	vecDir = m_pPlayer->FireBulletsPlayer(1, vecSrc, vecAiming, Vector(flSpread, flSpread, flSpread), 8192, BULLET_PLAYER_9MM, 0, 0, m_pPlayer->pev, m_pPlayer->random_seed);
+
+	PLAYBACK_EVENT_FULL(flags, m_pPlayer->edict(), m_usFireUsp, 0.0, g_vecZero, g_vecZero, vecDir.x, vecDir.y, 0, 0, (m_iClip == 0) ? 1 : 0, 0);
+
+	m_flNextPrimaryAttack = m_flNextSecondaryAttack = GetNextAttackDelay(flCycleTime);
+
+	if (0 == m_iClip && m_pPlayer->m_rgAmmo[m_iPrimaryAmmoType] <= 0)
+		// HEV suit - indicate out of ammo condition
+		m_pPlayer->SetSuitUpdate("!HEV_AMO0", false, 0);
+
+	m_flTimeWeaponIdle = UTIL_WeaponTimeBase() + UTIL_SharedRandomFloat(m_pPlayer->random_seed, 10, 15);
+}
+
+
+void CUsp::Reload()
+{
+	if (m_pPlayer->ammo_9mm <= 0)
+		return;
+
+	bool iResult;
+
+	if (m_iClip == 0)
+		iResult = DefaultReload(17, USP_RELOAD, 1.5);
+	else
+		iResult = DefaultReload(17, USP_RELOAD_NOT_EMPTY, 1.5);
+
+	if (iResult)
+	{
+		m_flTimeWeaponIdle = UTIL_WeaponTimeBase() + UTIL_SharedRandomFloat(m_pPlayer->random_seed, 10, 15);
+	}
+}
+
+
+
+void CUsp::WeaponIdle()
+{
+	ResetEmptySound();
+
+	m_pPlayer->GetAutoaimVector(AUTOAIM_10DEGREES);
+
+	if (m_flTimeWeaponIdle > UTIL_WeaponTimeBase())
+		return;
+
+	// only idle if the slid isn't back
+	if (m_iClip != 0)
+	{
+		int iAnim{};
+		float flRand = UTIL_SharedRandomFloat(m_pPlayer->random_seed, 0.0, 1.0);
+
+		if (flRand <= 0.3 + 0 * 0.75)
+		{
+			iAnim = USP_IDLE;
+			m_flTimeWeaponIdle = UTIL_WeaponTimeBase() + 11.0f / 16.0f;
+		}
+		SendWeaponAnim(iAnim);
+	}
+}
